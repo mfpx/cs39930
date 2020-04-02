33694cc6c04c8018ae71e380408c839bbecea95a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-02 22:10:09 +0100

    Heading fixed

---
+++ system/elements/heading.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="<?php echo $_SESSION['language']; ?>">
+    <head>
+        <link rel="stylesheet" href="css/pages.css">
+        <title><?php echo $title; ?></title>
+        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript">
+            function do_login()
+            {
+                var email = $("#email").val();
+                var pass = $("#password").val();
+                if (email !== "" && pass !== "")
+                {
+                    $("#loading").css({"display": "block"});
+                    $.ajax
+                            ({
+                                type: 'post',
+                                url: 'login.php',
+                                data: {
+                                    do_login: "do_login",
+                                    email: email,
+                                    password: pass
+                                },
+                                success: function (response) {
+                                    alert (response);
+                                    if (response === '1')
+                                    {
+                                        window.location.href = "index.php";
+                                        alert("success!");
+                                    } else
+                                    {
+                                        $("#loading").css({"display": "none"});
+                                        alert("Wrong Details");
+                                    }
+                                }
+                            });
+                } else {
+                    alert("Please Fill All The Details");
+                }
+                return false;
+            }
+        </script>
+
+    </head>
+    <body>
+        <div id="heading">
+            <a href="index.php">
+                <?php echo $title; ?>
+            </a>
+        </div>
+        <hr>
+        <div id="menu">
+            <a href="index.php"><?php echo $home; ?></a>
+            <a href="test.php">Test page</a>
+            <a href="test.php">Test page 2</a>
+            <a href="test.php">Test page 3</a>
+            <a href="test.php">Test page 4</a>
+        </div>
+    </body>
+</html>
