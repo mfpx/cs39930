8b70a8072d7c74852cbdf4a692a96a04ce8cce3c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-02 22:17:10 +0100

    Heading fixed

--- project/system/elements/heading.php
+++ project/system/elements/heading.php
@@ -44,17 +44,17 @@
     </head>
     <body>
         <div id="heading">
-            <a href="index">
+            <a href="index.php">
                 <?php echo $title; ?>
             </a>
         </div>
         <hr>
         <div id="menu">
-            <a href="index"><?php echo $home; ?></a>
-            <a href="test">Test page</a>
-            <a href="test">Test page 2</a>
-            <a href="test">Test page 3</a>
-            <a href="test">Test page 4</a>
+            <a href="index.php"><?php echo $home; ?></a>
+            <a href="test.php">Test page</a>
+            <a href="test.php">Test page 2</a>
+            <a href="test.php">Test page 3</a>
+            <a href="test.php">Test page 4</a>
         </div>
     </body>
 </html>
